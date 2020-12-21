bb6f012f4dfe256356401d3adeb22baf956c4abf
Vaibhav <Vaibhavpanwar1402@gmail.com>
2020-12-21 14:11:16 +0530

    lag gaye

--- backend/server.js
+++ backend/server.js
@@ -28,18 +28,18 @@ app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
 
-// tp use dirname in es7  syntaxt , dirname works  in common js syntax
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve((__dirname, "frontend", "build", "index.html")));
-  });
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
   app.get("/", (req, res) => {
-    res.send("Api running haha");
+    res.send("API is running....");
   });
 }
 
